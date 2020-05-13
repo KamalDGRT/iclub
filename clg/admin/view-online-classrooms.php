b95a5df728c505035ab4478ecc3112524701d6d9
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-13 19:02:07 +0530

    Update view-online-classrooms.php
    Added the table to show the online classrooms

--- clg/admin/view-online-classrooms.php
+++ clg/admin/view-online-classrooms.php
@@ -13,8 +13,10 @@ include('include/functions.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Admin | View Online Classrooms</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/tablestyle.css">
 </head>
 
 <body id="page-top">
@@ -25,13 +27,76 @@ include('include/functions.php');
             <div id="content">
 
                 <?php include('include/header.php'); ?>
+
                 <div class="container-fluid">
-                    <div class="text-center mt-5">
-                        <div class="error mx-auto" data-text="404">
-                            <p class="m-0">404</p>
+                    <div class="card shadow">
+                        <div class="card-header py-3">
+                            <p class="text-primary m-0 font-weight-bold">Online Classrooms in the Institution</p>
                         </div>
-                        <p class="text-dark mb-5 lead">Page Not Found</p>
-                        <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="index.php">← Back to Dashboard</a>
+                        <div class="card-body">
+
+                            <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                                <table class="table dataTable my-0" id="onlineclass">
+                                    <thead>
+                                        <tr>
+                                            <th>S.No.</th>
+                                            <th>Batch</th>
+                                            <th>Programme</th>
+                                            <th>Semester</th>
+                                            <th>Platform</th>
+                                            <th>Classroom&nbsp;Code</th>
+                                            <th>Options</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>2018-2022</td>
+                                            <td>B.Tech. I.T.</td>
+                                            <td>4</td>
+                                            <td>Google</td>
+                                            <td>dsalkfj</td>
+                                            <td><a href="#" class="btn-xs tooltips" tooltip-placement="top" tooltip="Edit">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <a href="#" onClick="return confirm('Are you sure you want to delete?')" class="btn-xs tooltips" tooltip-placement="top" tooltip="Remove">
+                                                    <i class="fa fa-times fa fa-white"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>2</td>
+                                            <td>2018-2022</td>
+                                            <td>B.Tech. I.T.</td>
+                                            <td>4</td>
+                                            <td>Edmodo</td>
+                                            <td>piuopew</td>
+                                            <td><a href="#" class="btn-xs tooltips" tooltip-placement="top" tooltip="Edit">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>&nbsp;&nbsp;&nbsp;&nbsp;
+                                                <a href="#" onClick="return confirm('Are you sure you want to delete?')" class="btn-xs tooltips" tooltip-placement="top" tooltip="Remove">
+                                                    <i class="fa fa-times fa fa-white"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>S.No.</th>
+                                            <th>Batch</th>
+                                            <th>Programme</th>
+                                            <th>Semester</th>
+                                            <th>Platform</th>
+                                            <th>Classroom&nbsp;Code</th>
+                                            <th>Options</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
@@ -43,7 +108,16 @@ include('include/functions.php');
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap.min.js"></script>
     <script src="assets/js/theme.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#onlineclass').DataTable({
+                "ordering": false
+            });
+        });
+    </script>
 </body>
 
 </html>
