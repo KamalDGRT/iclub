15414457323619da36791201452c7ef7bbdf1975
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-21 21:14:25 +0530

    [admin] [view-online-classrooms.php] Making the text colour in table to black

--- clg/admin/view-online-classrooms.php
+++ clg/admin/view-online-classrooms.php
@@ -36,7 +36,7 @@ include('include/functions.php');
                         <div class="card-body">
 
                             <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                                <table class="table dataTable my-0" id="onlineclass">
+                                <table class="table dataTable my-0 black" id="onlineclass">
                                     <thead>
                                         <tr>
                                             <th>S.No.</th>
