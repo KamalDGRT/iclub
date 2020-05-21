c2936f7d0e8c30630b4bc4cab667486b97f01000
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-21 21:15:16 +0530

    [admin] [user-session-logs.php] Making the text colour in table to black

--- clg/admin/user-session-logs.php
+++ clg/admin/user-session-logs.php
@@ -36,7 +36,7 @@ include('include/functions.php');
                         <div class="card-body">
 
                             <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                                <table class="table dataTable my-0" id="userlog">
+                                <table class="table dataTable my-0 black" id="userlog">
                                     <thead>
                                         <?php displayTableHeadUserLogs(); ?>
                                     </thead>
