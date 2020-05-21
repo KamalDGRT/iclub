9320a3ee3dc48cc8d490947a543cd025bec359b6
Kamal Sharma <kamaldgrt@gmail.com>
2020-05-21 21:13:00 +0530

    [admin] [view-time-table.php] Making the text colour in table to black

--- clg/admin/view-time-table.php
+++ clg/admin/view-time-table.php
@@ -36,7 +36,7 @@ include('include/functions.php');
                         <div class="card-body">
 
                             <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                                <table class="table dataTable my-0 myText myFont" id="viewtt">
+                                <table class="table dataTable my-0 black" id="viewtt">
                                     <thead class="h6 myFont">
                                         <tr>
                                             <th>Days</th>
